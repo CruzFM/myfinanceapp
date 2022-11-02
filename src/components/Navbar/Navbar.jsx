6d7f27ce3e2528a52e5aeb206f328b4bb50c4299
Fer Cruz <cruzfernandomartin94@gmail.com>
2022-11-02 01:20:07 -0300

    links added to the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import "./navbar.style.css";
+import { Link } from "react-router-dom";
 
 export const Navbar = ()=>{
     return(
         <nav className="navbar">
-            <span>FinanceApp</span>
+            <span><Link to="/">FinanceApp</Link></span>
             <ul>
-                <li>Incomes</li>
-                <li>Spent</li>
-                <li>Savings</li>
+                <li><Link to="/incomes">Incomes</Link></li>
+                <li><Link to="/spent">Spent</Link></li>
+                <li><Link to="/savings">Savings</Link></li>
             </ul>
         </nav>
     )
