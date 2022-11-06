03577726d245941376746e25b9d676a663ac96ca
Fer Cruz <cruzfernandomartin94@gmail.com>
2022-11-06 02:52:03 -0300

    card styling fixed. footer styling fixed.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,8 +4,8 @@ export const Card = (props) =>{
     return(
         <div className="card">
             <h2>${props.data.amount}</h2>
-            <h3>{props.data.date}</h3>
-            <h3>{props.data.category}</h3>
+            <h4>{props.data.date}</h4>
+            <h6>{props.data.category}</h6>
             <p>{props.data.detail}</p>
         </div>
     )
