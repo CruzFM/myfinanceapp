67d2dd1dfcbfd875427a96732e735a3a90ed485e
Fer Cruz <cruzfernandomartin94@gmail.com>
2022-11-14 23:23:02 -0300

    minor style change in forms

--- src/components/FormAdd/FormAdd.jsx
+++ src/components/FormAdd/FormAdd.jsx
@@ -37,7 +37,7 @@ export const FormAdd = (props)=> {
                     <div>
                         <label htmlFor="amount">Amount</label>
                         <Field name='amount' type='number' placeholder='Example: 777' />
-                        {errors.amount && touched.amount && <div>{errors.amount}</div>}
+                        {errors.amount && touched.amount && <div className="errors">{errors.amount}</div>}
                     </div>
                     <div>
                         <label htmlFor="category">Category</label>
@@ -45,19 +45,19 @@ export const FormAdd = (props)=> {
                             <option value=''>Categories</option>
                             {props.category.map(category => <option value={category} key={category}>{category}</option>)}
                         </Field>
-                        {errors.category && touched.category && <div>{errors.category}</div>}
+                        {errors.category && touched.category && <div className="errors">{errors.category}</div>}
                     </div>
                     <div>
                         <label htmlFor="detail">Detail</label>
                         <Field name='detail' type='input' placeholder='Your details here' />
-                        {errors.detail && touched.detail && <div>{errors.detail}</div>}
+                        {errors.detail && touched.detail && <div className="errors">{errors.detail}</div>}
                     </div>
                     <div>
                         <label htmlFor="date">Date</label>
                         <Field name='date' type='date'  />
-                        {errors.date && touched.date && <div>{errors.date}</div>}
+                        {errors.date && touched.date && <div className="errors">{errors.date}</div>}
                     </div>
-                    <button type="submit">Add {props.nameAdd}</button>
+                    <button type="submit" className="form--submitBtn">Add {props.nameAdd}</button>
 
                 </Form>)
                 }
